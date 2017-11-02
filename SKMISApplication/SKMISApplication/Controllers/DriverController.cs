3d3f414ac96e7f836e7255580408580f5a6dd1a5
Satyam Gupta <saatyyammgupta87@gmail.com>
2017-11-02 13:32:38 +0530

    Added Driver module

--- SKMISApplication/SKMISApplication/Controllers/DriverController.cs
+++ SKMISApplication/SKMISApplication/Controllers/DriverController.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SKMISApplication.Models;
-using System.Threading.Tasks;
 
 namespace SKMISApplication.Controllers
 {
@@ -19,31 +19,39 @@ namespace SKMISApplication.Controllers
         public async Task<ActionResult> Index()
         {
             DriverEntryModel _dEntry = new DriverEntryModel();
-            var result = (from driver in db.DriverEntries
-                          where driver.IsActive == true
-                          select new
+            var result = (from dvr in db.DriverEntries
+                          where dvr.IsActive == true
+                          select dvr).ToList().OrderByDescending(a => a.DriverId)
+                          .Select(b => new DriverEntryModel()
                           {
-                              ID = driver.DriverId,
-                              DriverName = driver.DriverName,
-                              DriverContactNumber = driver.DriverContactNumber
-                          }).ToList()
-                          .Select(d => new DriverEntryModel()
-                          {
-                              DriverId = d.ID,
-                              DriverName = d.DriverName,
-                              DriverContactNumber = d.DriverContactNumber
+                              DriverId = b.DriverId,
+                              DriverName = b.DriverName,
+                              DriverContactNumber = b.DriverContactNumber,
+                              WorkContactNumber = b.WorkContactNumber,
+                              EmailID = b.EmailID,
+                              ImagePath = b.ImagePath,
+                              LicenseNo = b.LicenseNo,
+                              LicenseExpiryDate = b.LicenseExpiryDate,
+                              LicenseState = b.LicenseState,
+                              DOB = b.DOB,
+                              AlternateContactNo = b.AlternateContactNo,
+                              DriverAddress = b.DriverAddress,
+                              City = b.City,
+                              Pin = b.Pin,
+                              Misc = b.Misc
                           });
             return View(result.ToList());
+            //return View(await db.DriverEntries.ToListAsync());
         }
 
         // GET: Driver/Details/5
-        public ActionResult Details(long? id)
+        public async Task<ActionResult> Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DriverEntry driverEntry = db.DriverEntries.Find(id);
+            DriverEntry driverEntry = await db.DriverEntries.FindAsync(id);
             if (driverEntry == null)
             {
                 return HttpNotFound();
@@ -59,7 +67,7 @@ namespace SKMISApplication.Controllers
 
         // POST: Driver/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(DriverEntryModel driverEntry)
@@ -67,15 +75,27 @@ namespace SKMISApplication.Controllers
             if (ModelState.IsValid)
             {
                 DriverEntry _dEntry = new DriverEntry();
+                _dEntry.DriverId = driverEntry.DriverId;
                 _dEntry.DriverName = driverEntry.DriverName;
                 _dEntry.DriverContactNumber = driverEntry.DriverContactNumber;
+                _dEntry.WorkContactNumber = driverEntry.WorkContactNumber;
+                _dEntry.EmailID = driverEntry.EmailID;
+                _dEntry.ImagePath = driverEntry.ImagePath;
+                _dEntry.LicenseNo = driverEntry.LicenseNo;
+                _dEntry.LicenseExpiryDate = driverEntry.LicenseExpiryDate;
+                _dEntry.LicenseState = driverEntry.LicenseState;
+                _dEntry.DOB = driverEntry.DOB;
+                _dEntry.AlternateContactNo = driverEntry.AlternateContactNo;
+                _dEntry.DriverAddress = driverEntry.DriverAddress;
+                _dEntry.City = driverEntry.City;
+                _dEntry.Pin = driverEntry.Pin;
+                _dEntry.Misc = driverEntry.Misc;
                 _dEntry.IsActive = true;
                 _dEntry.CreatedDate = DateTime.Now;
                 db.DriverEntries.Add(_dEntry);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-
             return View(driverEntry);
         }
 
@@ -86,35 +106,42 @@ namespace SKMISApplication.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            DriverEntryModel _driverEntry = new DriverEntryModel();
-            var result = (from driver in db.DriverEntries
-                          where driver.IsActive == true && driver.DriverId == id
-                          select new
-                          {
-                              ID = driver.DriverId,
-                              DriverName = driver.DriverName,
-                              DriverContactNumber = driver.DriverContactNumber
-                          }).ToList()
-             .Select(d => new DriverEntryModel()
+            DriverEntryModel driverEntry = new DriverEntryModel();
+            var result = (from dvr in db.DriverEntries
+                          where dvr.IsActive == true && dvr.DriverId == id
+                          select dvr).ToList()
+             .Select(b => new DriverEntryModel()
              {
-                 DriverId = d.ID,
-                 DriverName = d.DriverName,
-                 DriverContactNumber = d.DriverContactNumber
+                 DriverId = b.DriverId,
+                 DriverName = b.DriverName,
+                 DriverContactNumber = b.DriverContactNumber,
+                 WorkContactNumber = b.WorkContactNumber,
+                 EmailID = b.EmailID,
+                 ImagePath = b.ImagePath,
+                 LicenseNo = b.LicenseNo,
+                 LicenseExpiryDate = b.LicenseExpiryDate,
+                 LicenseState = b.LicenseState,
+                 DOB = b.DOB,
+                 AlternateContactNo = b.AlternateContactNo,
+                 DriverAddress = b.DriverAddress,
+                 City = b.City,
+                 Pin = b.Pin,
+                 Misc = b.Misc
              });
-            _driverEntry = result.FirstOrDefault();
-            if (_driverEntry == null)
+            driverEntry = result.FirstOrDefault();
+            if (driverEntry == null)
             {
                 return HttpNotFound();
             }
-            return View(_driverEntry);
+            return View(driverEntry);
         }
 
         // POST: Driver/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(DriverEntry driverEntry)
+        public async Task<ActionResult> Edit(DriverEntryModel driverEntry)
         {
             if (ModelState.IsValid)
             {
@@ -122,6 +149,18 @@ namespace SKMISApplication.Controllers
                 _dEntry.DriverId = driverEntry.DriverId;
                 _dEntry.DriverName = driverEntry.DriverName;
                 _dEntry.DriverContactNumber = driverEntry.DriverContactNumber;
+                _dEntry.WorkContactNumber = driverEntry.WorkContactNumber;
+                _dEntry.EmailID = driverEntry.EmailID;
+                _dEntry.ImagePath = driverEntry.ImagePath;
+                _dEntry.LicenseNo = driverEntry.LicenseNo;
+                _dEntry.LicenseExpiryDate = driverEntry.LicenseExpiryDate;
+                _dEntry.LicenseState = driverEntry.LicenseState;
+                _dEntry.DOB = driverEntry.DOB;
+                _dEntry.AlternateContactNo = driverEntry.AlternateContactNo;
+                _dEntry.DriverAddress = driverEntry.DriverAddress;
+                _dEntry.City = driverEntry.City;
+                _dEntry.Pin = driverEntry.Pin;
+                _dEntry.Misc = driverEntry.Misc;
                 _dEntry.UpdatedDate = DateTime.Now;
                 db.Entry(_dEntry).State = EntityState.Modified;
                 await db.SaveChangesAsync();
@@ -138,12 +177,11 @@ namespace SKMISApplication.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             DriverEntry driverEntry = await db.DriverEntries.FindAsync(id);
-            driverEntry.DriverId = (long)id;
-            driverEntry.IsActive = false;
-            driverEntry.UpdatedDate = DateTime.Now;
-            db.Entry(driverEntry).State = EntityState.Modified;
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            if (driverEntry == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driverEntry);
         }
 
         // POST: Driver/Delete/5
@@ -152,9 +190,7 @@ namespace SKMISApplication.Controllers
         public async Task<ActionResult> DeleteConfirmed(long id)
         {
             DriverEntry driverEntry = await db.DriverEntries.FindAsync(id);
-            driverEntry.DriverId = id;
-            driverEntry.IsActive = false;
-            db.Entry(driverEntry).State = EntityState.Modified;
+            db.DriverEntries.Remove(driverEntry);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
--- SKMISApplication/SKMISApplication/Models/DriverEntryModel.cs
+++ SKMISApplication/SKMISApplication/Models/DriverEntryModel.cs
@@ -9,8 +9,40 @@ namespace SKMISApplication.Models
     public class DriverEntryModel
     {
         public long DriverId { get; set; }
-        [Required]
+
+        [Display(Name ="Name")]
         public string DriverName { get; set; }
-        public long? DriverContactNumber { get; set; }
+        [Display(Name = "Personal No")]
+        public Nullable<long> DriverContactNumber { get; set; }
+        [Display(Name = "Work M.No")]
+        public Nullable<long> WorkContactNumber { get; set; }
+        [Display(Name = "Email ID")]
+        public string EmailID { get; set; }
+        [Display(Name = "Upload D.L.")]
+        public string ImagePath { get; set; }
+        [Display(Name = "License")]
+        public string LicenseNo { get; set; }
+
+        [Display(Name = "L. Issue State")]
+        public string LicenseState { get; set; }
+
+        [Display(Name = "L. Expiry Date")]
+        //[DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? LicenseExpiryDate { get; set; }
+
+        [Display(Name = "D.O.B.")]
+        //[DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? DOB { get; set; }
+
+        [Display(Name = "Alt. M.No")]
+        public Nullable<long> AlternateContactNo { get; set; }
+        [Display(Name = "Address")]
+        public string DriverAddress { get; set; }
+        public string City { get; set; }
+        public string Pin { get; set; }
+        [Display(Name = "Notes")]
+        public string Misc { get; set; }
     }
 }
