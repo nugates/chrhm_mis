627ecdbdcf67b714e7f5bcd44619dba3fd7fc4a6
Rakesh Baitha <rak.baitha@gmail.com>
2017-11-03 15:13:22 +0530

    Integrated chnages and mmerge with latest updates 03/11/2017

---
+++ SKMISApplication/SKMISApplication/Models/ListFiller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SKMISApplication.Models
+{
+    public class ListFiller
+    {
+    }
+    public class Ward
+    {
+        public long ID { get; set; }
+        public string WardName { get; set; }
+    }
+    public class OwnerType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class HouseCategory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class StructureType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class Caste
+    {
+        public long ID { get; set; }
+        public string CasteName { get; set; }
+    }
+    public class Document
+    {
+        public long ID { get; set; }
+        public string DocumentName { get; set; }
+    }
+    public class Name
+    {
+        public long ID { get; set; }
+        public string FullName { get; set; }
+    }
+    public class Title
+    {
+        public long ID { get; set; }
+        public string NOCTitle { get; set; }
+    }
+    public class House
+    {
+        public long ID { get; set; }
+        public string HouseNumber { get; set; }
+    }
+    public class Occupation
+    {
+        public long ID { get; set; }
+        public string OccupationTitle { get; set; }
+    }
+    public class Qualification
+    {
+        public long ID { get; set; }
+        public string QualificationName { get; set; }
+    }
+    public class Disability
+    {
+        public long ID { get; set; }
+        public string DisabilityType { get; set; }
+    }
+}
--- SKMISApplication/SKMISApplication/Models/SKMIS.Context.cs
+++ SKMISApplication/SKMISApplication/Models/SKMIS.Context.cs
@@ -25,11 +25,19 @@ namespace SKMISApplication.Models
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<BeneficiaryDocument> BeneficiaryDocuments { get; set; }
         public virtual DbSet<BeneficiaryEntry> BeneficiaryEntries { get; set; }
+        public virtual DbSet<BeneficiaryEntry_old> BeneficiaryEntry_old { get; set; }
+        public virtual DbSet<CasteMaster> CasteMasters { get; set; }
+        public virtual DbSet<ConstituencyMaster> ConstituencyMasters { get; set; }
+        public virtual DbSet<DisabilityMaster> DisabilityMasters { get; set; }
         public virtual DbSet<DriverEntry> DriverEntries { get; set; }
         public virtual DbSet<EmployeeEntry> EmployeeEntries { get; set; }
         public virtual DbSet<InventoryEntry> InventoryEntries { get; set; }
+        public virtual DbSet<OccupationMaster> OccupationMasters { get; set; }
+        public virtual DbSet<QualificationMaster> QualificationMasters { get; set; }
         public virtual DbSet<VehicleEntry> VehicleEntries { get; set; }
         public virtual DbSet<VendorEntry> VendorEntries { get; set; }
+        public virtual DbSet<WardMaster> WardMasters { get; set; }
     }
 }
